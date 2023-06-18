d9242eb365c2664ced177e5f064fa8d3ede52bbc
papun kumar <papunkumar101>
2023-06-18 20:30:42 +0530

    second commit

--- FirstNodeApp/project3-rest-api/index.js
+++ FirstNodeApp/project3-rest-api/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyPerser = require('body-parser');
 const app = express();
 const courseRoutes = require('./routes/course');
 require('dotenv').config();
@@ -8,9 +10,11 @@ const config = process.env;
 //     res.send('This is the get request of express js library...updated');
 // }).listen(2000); 32
 
-
+app.use(bodyPerser.json());
 app.use('/api/v1/courses',courseRoutes);
 
+mongoose.connect(config.DB_CONNECTION_URL);
+console.log('database connect successfully.');
 
 app.listen(config.PORT,()=>{
     console.log('project start....');
--- FirstNodeApp/project3-rest-api/routes/course.js
+++ FirstNodeApp/project3-rest-api/routes/course.js
@@ -1,10 +1,55 @@
 const express = require('express');
-
 const router = express.Router();
+const courses = require('../models/course');
 
 
-router.get('/',(req,res)=>{
-    res.send('here is your all courcess');
+router.get('/',async (req,res)=>{
+    // res.send('here is your all courcess'); 
+    try {
+        const crs = await courses.find();
+        res.json(crs);
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+router.get('/:courseId',async (req,res)=>{ 
+    const courseId = req.params.courseId;
+    try {
+        const crs = await courses.findById(courseId);
+        res.json(crs);
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+router.post('/',async (req,res)=>{  
+    try {
+        const crs = await courses.create(req.body);
+        res.json(crs);
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+router.delete('/:courseId',async (req,res)=>{
+    // const courseId = req.params.courseId;
+    try{
+        await courses.deleteOne({"_id":req.params.courseId});
+        res.status(200).json({"message":"done"});
+    }catch(error){
+        res.json({"custom-message":"something wrong",error});
+    }
+});
+
+router.put('/:courseId',async (req,res)=>{
+    const courseId = req.params.courseId;
+    try{
+        const crs = await courses.updateOne({"_id":courseId},req.body);
+        res.json(crs);
+    }catch(error){
+        res.json(error);
+    }
 });
 
 module.exports = router;
